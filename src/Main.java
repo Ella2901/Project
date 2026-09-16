ec3b74540a51f18f902a8ea4e0678585268210f2
Le Chi Chau <lechi.chau@students.mq.edu.au>
2026-09-16 10:00:04 +1000

    Catch the exception in Main

--- src/Main.java
+++ src/Main.java
@@ -24,12 +24,22 @@ public class Main {
         robot.move(5);
         robot.display();
 
-        Grid<Entity> grid = new Grid<>();
+        Grid<Entity> grid = new Grid<>(800, 600);
 
         grid.addEntity(robot);
         grid.addEntity(package1);
         grid.addEntity(chargingStation);
-        
+
         grid.displayEntities();
+
+        Warehouse warehouse = new Warehouse();
+        warehouse.addEntity(robot);
+
+        try {
+            warehouse.moveRobot(robot, 50);
+            robot.display();
+        } catch (InvalidPositionException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
